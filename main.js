4c52408141653422c01c238c0808d8363f959bc8
Garg_ Shobhit (shogarg@deloitte.com) <shogarg@your.hostname.com>
2023-05-09 12:32:12 +0530

    learn about querySelector

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-function Zomato(DeliveryPerson) {
-  console.log("Shobhit Placed a order!");
-  DeliveryPerson();
-}
-Zomato(function DeliveryPerson() {
-  console.log("Could you please deliver food?");
-});
+const para = document.querySelector('p')
+console.log(para)
+
+// Getting element via class name
+const detail = document.querySelector('.detail')
+console.log(detail)
+
+// We can get the selector by inspecting an element in browser
+const inspectedSelectorName = document.querySelector('body > div:nth-child(1) > div:nth-child(4)')
+console.log(inspectedSelectorName)
+
+// Selecting multiple elements at once
+const paras = document.querySelectorAll('p')
+console.log(paras)
