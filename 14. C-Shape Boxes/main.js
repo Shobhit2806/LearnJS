abcbf6f2000513bcf398f47b2342aa7baedd91d5
Garg_ Shobhit (shogarg@deloitte.com) <shogarg@your.hostname.com>
2023-09-14 11:44:00 +0530

    implementing event delegation in c-shape box qs

--- 14. C-Shape Boxes/main.js
+++ 14. C-Shape Boxes/main.js
@@ -1,24 +1,49 @@
 const boxes = document.querySelectorAll(".col");
+const boxContainer = document.querySelector(".box-container");
 
 let queue = [];
 
-boxes.forEach((box) => {
-  box.addEventListener("click", function (e) {
-    // let index = e.target.dataset.index;
-    queue.push(box);
-    box.style.backgroundColor = "green";
+//Implementing event delegation in this
 
-    if (queue.length === 7) {
-      let cnt = 0;
-      for (let i = 0; i < queue.length; i++) {
-        setTimeout(() => {
-          queue[i].style.backgroundColor = "white";
-          cnt++;
-          if (cnt === 7) {
-            queue = [];
-          }
-        }, 500 * i);
-      }
+boxContainer.addEventListener("click", function (e) {
+  let index = e.target.dataset.index;
+  if (index != undefined) {
+    e.target.style.backgroundColor = "green";
+
+    queue.push(index);
+  }
+
+  if (queue.length === 7) {
+    let cnt = 0;
+    for (let i = 0; i < queue.length; i++) {
+      setTimeout(() => {
+        boxes[queue[i]].style.backgroundColor = "white";
+        cnt++;
+        if (cnt === 7) {
+          queue = [];
+        }
+      }, 500 * i);
     }
-  });
+  }
 });
+
+// boxes.forEach((box) => {
+//   box.addEventListener("click", function (e) {
+//     // let index = e.target.dataset.index;
+//     queue.push(box);
+//     box.style.backgroundColor = "green";
+
+//     if (queue.length === 7) {
+//       let cnt = 0;
+//       for (let i = 0; i < queue.length; i++) {
+//         setTimeout(() => {
+//           queue[i].style.backgroundColor = "white";
+//           cnt++;
+//           if (cnt === 7) {
+//             queue = [];
+//           }
+//         }, 500 * i);
+//       }
+//     }
+//   });
+// });
